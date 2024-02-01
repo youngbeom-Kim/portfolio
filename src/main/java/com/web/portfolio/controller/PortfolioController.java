8d55e3031b8664f77f53e90abb3b31aeda1521d7
youngbeom-Kim <abcd450196@gmail.com>
2024-02-01 18:26:43 +0900

    feat: application.properties 삭제 후 application.yml 추가 thymeleaf 설정 및 header 추가

---
+++ src/main/java/com/web/portfolio/controller/PortfolioController.java
@@ -0,0 +1,14 @@
+package com.web.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PortfolioController {
+
+    @GetMapping("main")
+    public String main() {
+        return "portfolio/main";
+    }
+
+}
